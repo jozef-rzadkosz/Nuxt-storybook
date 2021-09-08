93d8a8594066def04d36b02fa9c623445b680255
freestyle09 <jozefrzadkosz09@gmail.com>
2021-09-08 14:28:58 +0200

    Added nuxt + ts + scss + storybook

---
+++ components/NuxtLogo.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class='nuxt-logo'>
+    <span class='nuxt-logo__title'>NuxtLogo</span>
+    <img class='nuxt-logo__image' :src='image' alt=''>
+    <code>{{multiply({x: 2, y: 15})}}</code>
+  </div>
+</template>
+
+<script lang='ts'>
+interface Numbers {
+  x: number,
+  y: number,
+}
+
+export default {
+  props: {
+    image: {
+      type: [String]
+    }
+  },
+  methods: {
+    multiply(coords: Numbers):number {
+      return coords.x * coords.y;
+    }
+  }
+}
+</script>
+
+<style lang='scss' scoped>
+.nuxt-logo {
+  height: 180px;
+  &__title{
+    font-size: 2rem;
+  }
+  &__image{
+    width: 500px;
+  }
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>Siema</h1>
+  </div>
+</template>
+
+<script lang="ts">
+export default {}
+</script>
+
+
